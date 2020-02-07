3f59b45cc706f3e2184e1943c794167d8dc6eaf5
Tóth Bence <toth.bence@encosoft.hu>
2020-02-07 17:44:28 +0100

    added shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
 import { AdminModule } from './admin/admin.module';
+import { SharedModule } from './shared/shared/shared.module';
 
 @Module({
-  imports: [ApiModule, AdminModule],
+  imports: [ApiModule, AdminModule, SharedModule],
   controllers: [AppController],
   providers: [AppService],
+  exports: [SharedModule]
 })
 export class AppModule {}
